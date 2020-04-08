19e493a984aadcad9f022d20fe7422ca5f4fde91
Yazan Tarifi <yazantarifi98@gmail.com>
2020-04-08 23:45:07 +0100

    finish setup

---
+++ src/androidMain/kotlin/sample/Sample.kt
@@ -0,0 +1,7 @@
+package sample
+
+actual class Sample actual constructor() {
+    actual fun checkMe(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
